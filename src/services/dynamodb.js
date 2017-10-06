de7246cba24254cbb156fa1d2d666672fa4db06b
Rafal Wilinski <raf.wilinski@gmail.com>
2017-10-06 17:12:29 +0200

    Create DynamoDB service

--- src/services/dynamodb.js
+++ src/services/dynamodb.js
@@ -1,6 +1,58 @@
 class DynamoDBService {
-  constructor (documentClient) {
+  constructor (documentClient, tableName) {
     this.db = documentClient;
+    this.tableName = tableName;
+  }
+
+  get (id) {
+    const params = {
+      TableName: this.tableName,
+      Key: {
+        id
+      }
+    };
+
+    return this.db
+      .get(params)
+      .promise()
+      .then(data => data.Item);
+  }
+
+  put (Item) {
+    if (!Item.id) {
+      return Promise.reject('Item.id is not defined!');
+    }
+
+    const params = {
+      TableName: this.tableName,
+      Item
+    };
+
+    return this.db.put(params).promise();
+  }
+
+  update (id, UpdateExpression, ExpressionAttributeValues) {
+    const params = {
+      TableName: this.tableName,
+      Key: {
+        id
+      },
+      UpdateExpression,
+      ExpressionAttributeValues
+    };
+
+    return this.db.update(params).promise();
+  }
+
+  delete (id) {
+    const params = {
+      TableName: this.tableName,
+      Key: {
+        id
+      }
+    };
+
+    return this.db.delete(params).promise();
   }
 }
 
