549f7c0b018a85f490059bba0182fa4dbd436842
Rafal Wilinski <raf.wilinski@gmail.com>
2017-10-06 19:43:28 +0200

    Add listVault functionality

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ const AWS = require('aws-sdk');
 const DocumentDB = new AWS.DynamoDB.DocumentClient();
 const slack = require('serverless-slack');
 const DynamoDBService = require('./services/dynamodb');
+
 const CoinsService = new DynamoDBService(DocumentDB, 'Coins');
+
 const GrantCoinsCommand = require('./commands/grantCoins');
+const ListCoinsCommand = require('./commands/listCoins');
 
 exports.handler = slack.handler.bind(slack);
 
 new GrantCoinsCommand(slack, CoinsService); // eslint-disable-line no-new
+new ListCoinsCommand(slack, CoinsService); // eslint-disable-line no-new
