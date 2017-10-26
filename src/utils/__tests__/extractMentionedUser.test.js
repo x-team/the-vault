132b21bcfb3799efc60e3bf26234079c6d3e6fbe
jacekelgda <jacekelgda@gmail.com>
2017-10-26 14:06:14 +0200

    Using excaped mentions

--- src/utils/__tests__/extractMentionedUser.test.js
+++ src/utils/__tests__/extractMentionedUser.test.js
@@ -1,13 +1,25 @@
-const extractMentionedUsers = require('./../extractMentionedUsers');
+const { extractMentionedUser, extractMentionedUsers } = require('./../extractMentionedUsers');
 
 test('it should extract user facts from message', () => {
-  const data = [ { userId: 'U7A9CF0SV', userName: 'raf.wilinski' } ];
-  const slackTeamUsers = [{id: "U7A9CF0SV", name:"raf.wilinski"}, {id: "U7A9CF2SV", name:"john.doe"}];
-  expect(extractMentionedUsers("Hello @raf.wilinski !", slackTeamUsers)).toEqual(data);
+  const text = "Hello <@U7A9CF0SH|jacek.lawniczak>!";
+  const data = {
+    userId: "U7A9CF0SH",
+    userName: "jacek.lawniczak"
+  };
+  expect(extractMentionedUser(text)).toEqual(data);
 });
 
-test('it should extract user facts from message - multiple', () => {
-  const data = [ { userId: 'U7A9CF0SV', userName: 'raf.wilinski' }, { userId: 'U7A9CF2SV', userName: 'jacek.lawniczak' } ];
-  const slackTeamUsers = [{id: "U7A9CF0SV", name:"raf.wilinski"}, {id: "U7A9CF2SV", name:"jacek.lawniczak"}];
-  expect(extractMentionedUsers("Hello @raf.wilinski @jacek.lawniczak !!!", slackTeamUsers)).toEqual(data);
+test('it should extract users facts from message', () => {
+  const text = "Hello <@U7A9CF0SH|jacek.lawniczak> <@U7A9CF0SV|raf.wilinski>!!";
+  const data = [
+    {
+      userId: "U7A9CF0SH",
+      userName: "jacek.lawniczak"
+    },
+    {
+      userId: "U7A9CF0SV",
+      userName: "raf.wilinski"
+    }
+  ];
+  expect(extractMentionedUsers(text)).toEqual(data);
 });
--- src/utils/extractMentionedUsers.js
+++ src/utils/extractMentionedUsers.js
@@ -1,13 +1,30 @@
-const extractMentionedUsers = (message, slackTeamUsers) => {
-  const users = message.match(/@[a-z0-9][a-z0-9._-]*/g);
-  const usersData = [];
+/*
+* Example message: "Hello <@U7A9CF0SV|raf.wilinski>!"
+*/
+const extractMentionedUser = message => {
+  const mentionPart = message.split('<')[1].split('>')[0];
+  const userId = mentionPart.split('|')[0].split('@')[1];
+  const userName = mentionPart.split('|')[1];
 
-  for (let i = 0; i < slackTeamUsers.length; i += 1) {
-      if (users.includes(`@${slackTeamUsers[i].name}`)) {
-        usersData.push({ userId: slackTeamUsers[i].id, userName: slackTeamUsers[i].name });
-      }
-  }
-  return usersData;
+  return {
+    userId,
+    userName
+  };
 };
 
-module.exports = extractMentionedUsers;
+const extractMentionedUsers = text => {
+  const regex = /<@([a-zA-Z0-9]+)\|([a-z0-9._-]+)>/g;
+  const matches = text.match(regex);
+  const usersData = [];
+
+  for (let i = 0; i < matches.length; i += 1) {
+    usersData.push(extractMentionedUser(matches[i]));
+  }
+
+  return usersData;
+}
+
+module.exports = {
+  extractMentionedUser,
+  extractMentionedUsers
+};
