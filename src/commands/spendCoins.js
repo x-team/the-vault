c723b8b25ef6d58f09fbce121d15b0d62e63b842
jacekelgda <jacekelgda@gmail.com>
2017-10-26 17:19:49 +0200

    Multiple users draw at once

--- src/commands/spendCoins.js
+++ src/commands/spendCoins.js
@@ -1,4 +1,4 @@
-const { extractMentionedUserAndCoins } = require('../utils/extractMentionedUsers');
+const { extractMentionedUsersAndCoins } = require('../utils/extractMentionedUsers');
 const isAdmin = require('../utils/isAdmin');
 const { notifyUserAboutCoinsSpent } = require('../services/slack');
 
@@ -12,21 +12,23 @@ class SpendCoinsCommand {
 
   init () {
     this.slack.on('/drawcoin', async (msg, bot) => {
-      const mentionedUser = await extractMentionedUserAndCoins(msg.text);
+      const users = await extractMentionedUsersAndCoins(msg.text);
 
       if (isAdmin(msg.user_name)) {
-        try {
-          const user = await this.coinsService.get(mentionedUser.userId);
-          if (!user || user.coins === 0 || user.coins < mentionedUser.coins) {
-            bot.replyPrivate('User does not have enough coins to subtract from');
-          } else {
-            await this.coinsService.update(mentionedUser.userId, 'SET coins = coins - :cost', { ':cost': mentionedUser.coins });
-            await notifyUserAboutCoinsSpent(user.name, mentionedUser.coins);
+        for (let i = 0; i < users.length; i += 1) {
+          try {
+            const user = await this.coinsService.get(users[i].userId);
+            if (!user || user.coins === 0 || user.coins < users[i].coins) {
+              bot.replyPrivate('User does not have enough coins to subtract from');
+            } else {
+              await this.coinsService.update(users[i].userId, 'SET coins = coins - :cost', { ':cost': users[i].coins });
+              await notifyUserAboutCoinsSpent(user.name, users[i].coins);
 
-            bot.replyPrivate(`Coin subtracted! ${user.name} now has ${user.coins - mentionedUser.coins} coins.`);
+              bot.replyPrivate(`Coin subtracted! ${user.name} now has ${user.coins - users[i].coins} :coin:.`);
+            }
+          } catch (error) {
+            bot.replyPrivate('Whoops! An Error occured!');
           }
-        } catch (error) {
-          bot.replyPrivate('Whoops! An Error occured!');
         }
       } else {
         bot.replyPrivate('You don\'t have a permission to do that!');
